6d5bae038ebbcadf1d670f424de8b7004dce8ffe
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-25 19:32:10 +0800

    fixed def

--- calculator.py
+++ calculator.py
@@ -2,10 +2,14 @@
 class calculator:
 #define operators
     def addition (self, integer1, integer2):
-        return integer1 + integer2   
+        sum =  integer1 + integer2
+        return sum   
     def subtraction(self, integer1, integer2):
-        return integer1 - integer2
+        difference = integer1 - integer2
+        return difference
     def multiplication(self, integer1, integer2):
-        return integer1 * integer2
+        product =  integer1 * integer2
+        return product
     def division(self, integer1, integer2):
-        return integer1 / integer2
+        quotient =  integer1 / integer2
+        return quotient
