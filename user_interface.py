e9efa2b4175b168e8c44644cbd0db46405d1e846
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-25 18:56:44 +0800

    added answer

--- user_interface.py
+++ user_interface.py
@@ -13,5 +13,20 @@ def integer2 (self):
 def what_operation (self):
     operation = input("What do you want the program to solve? 1 - Addition. 2 - Subtraction. 3 - Multiplication. 4- Division. ")
     return operation
-#show answer
+# show answer for addition
+def sum_operation (self, sum):
+    print("The sum of " , integer1, "and ", integer2, "is ", addition(integer1, integer2))
+    
+# show output for subtraction
+def difference_operation (self, difference):
+    print("The difference of " , integer1, "and ", integer2, "is ", subtraction(integer1, integer2))
+
+# define multiplication functions
+def product_operation (self, product):
+    print("The product of " , integer1, "and ", integer2, "is ", multiplication(integer1, integer2))
+
+# define division function
+def quotient_operation (self, quotient):
+    print("The quotient of " , integer1, "and ", integer2, "is ", division(integer1, integer2))
+
 #ask to try again
