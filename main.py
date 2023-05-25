a0b9708129759ac0449ebb7f7143c9a60d301166
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-25 20:51:47 +0800

    revision

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #import user_interface and calculator
 from user_interface import userinterface 
 from calculator import calculator
+
+interface = userinterface()
+calc = calculator()
 #user input if they want to try again
 #ask operation
 #adding value error and zero division error
