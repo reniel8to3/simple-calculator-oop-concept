5d02315e0822c2eacaf33b2d27c4e90ae49aa3f2
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-25 18:33:39 +0800

    Added print statement

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,7 @@
 #user interface
 #print statement
+def print_statement(self):
+    print ("This program is designed to function as a simple calculator. This program can perform Addition, Subtraction, Multiplication, and Division.")
 #ask user for input
 #ask for operation
 #show answer
