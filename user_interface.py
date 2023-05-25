3e3892fb86ee12566d61464515df548b2ba99962
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-25 18:40:54 +0800

    added input

--- user_interface.py
+++ user_interface.py
@@ -3,6 +3,12 @@
 def print_statement(self):
     print ("This program is designed to function as a simple calculator. This program can perform Addition, Subtraction, Multiplication, and Division.")
 #ask user for input
+def integer1 (self):
+    int(input('Input the first integer: '))
+    return integer1
+def integer2 (self):
+    int(input('Input the second integer: '))
+    return integer2
 #ask for operation
 #show answer
 #ask to try again
