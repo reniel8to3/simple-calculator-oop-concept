aa62afdefe4b857987b059875cc63e6ad063c9d4
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-25 19:26:43 +0800

    added class and operators

--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
 #create class
+class calculator:
 #define operators
+    def add_input (self, integer1, integer2):
+        return integer1 + integer2   
+    def subtraction(self, integer1, integer2):
+        return integer1 - integer2
+    def multiplication(self, integer1, integer2):
+        return integer1 * integer2
+    def division(self, integer1, integer2):
+    return integer1 / integer2
