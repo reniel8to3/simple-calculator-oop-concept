955ffa41ff87214e124c4311aadbf9755b368dbe
reniel8to3 <renielalmariomalabo@gmail.com>
2023-06-04 20:23:05 +0800

    class

--- inheritance.py
+++ inheritance.py
@@ -1,5 +1,15 @@
 #import calculator file
 from calculator import calculator
 #make class
+class renielcalculator(calculator):
 #inherit
+    def __init__(self):
+        super().__init__()
 #more operators
+    def remainder_operation(self, integer1, integer2):
+            remainder = integer1 % integer2
+            return remainder
+
+    def exponent_operation (self, integer1, integer2):
+            exponent = integer1 ** integer2
+            return exponent
